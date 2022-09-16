7b19af5553e257f3a57717784f4a5d97a4633851
Tristan Elliott <>
2022-09-16 19:42:45 -0300

    create and read funtionality completed

---
+++ app/src/main/java/com/elliottsoftware/calfbook/util/CalfUtil.kt
@@ -0,0 +1,38 @@
+package com.elliottsoftware.calfbook.util
+
+import android.widget.EditText
+import android.widget.RadioButton
+
+class CalfUtil {
+
+    companion object {
+        /**
+         *  utility function to determine if the tag number is empty or not
+         * @param[tagNumber] the tag number entered by teh user
+         * @return boolean detecting if the tag number is empty
+         */
+        fun validateTagNumber(tagNumber: String, editTag: EditText): Boolean {
+            //if statements are expressions in Kotlin
+            return if (tagNumber.isEmpty()) {
+                editTag.error = "Field can not be empty"
+                true;
+            } else {
+                false;
+            }
+        }
+
+        /**
+         *  utility function to determine the sex of the calf
+         * @param[radioButton] the bull radio button to determine if it was
+         * clicked of not
+         * @return the sex of the calf to be saved
+         */
+        fun buttonIsChecked(radioButton: RadioButton):String{
+            return if(radioButton.isChecked){
+                "Bull"
+            }else{
+                "Heifer"
+            }
+        }
+    }
+}
