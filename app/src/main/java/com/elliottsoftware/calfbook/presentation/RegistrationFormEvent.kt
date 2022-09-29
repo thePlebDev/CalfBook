fe31ecb4b9fa38da52bb2530890390d63eecd368
Tristan Elliott <>
2022-09-29 12:46:03 -0300

    loginViewModel complete

--- app/src/main/java/com/elliottsoftware/calfbook/presentation/RegistrationFormEvent.kt
+++ app/src/main/java/com/elliottsoftware/calfbook/presentation/RegistrationFormEvent.kt
@@ -1,4 +1,11 @@
 package com.elliottsoftware.calfbook.presentation
 
+import com.elliottsoftware.calfbook.domain.use_cases.ValidatePassword
+
 sealed class RegistrationFormEvent {
+    //These are all subclasses
+    data class EmailChanged(val email:String):RegistrationFormEvent()
+    data class PasswordChanged(val password: String): RegistrationFormEvent()
+
+    object Submit: RegistrationFormEvent()
 }
